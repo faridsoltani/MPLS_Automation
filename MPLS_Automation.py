f384f6cf42e19b00e898fa158cbab76cedbba141
faridsoltani <112269606+faridsoltani@users.noreply.github.com>
2022-08-28 11:23:46 +0430

    Add files via upload

---
+++ MPLS_Automation.py
@@ -0,0 +1,13 @@
+#MPLS Automation Code 
+Router_LSR_ID=input("Enter Router LSR-ID : ")
+print("Here is your code : ")
+print("""
+mpls lsr-id {0}
+
+
+
+
+
+
+""".format(Router_LSR_ID))
+input("Press enter to exit")
