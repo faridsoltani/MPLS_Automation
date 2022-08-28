1efff640eabd962bf727be2c8136a7e22e2f2130
faridsoltani <112269606+faridsoltani@users.noreply.github.com>
2022-08-28 11:33:30 +0430

    Update MPLS_Automation.py

--- MPLS_Automation.py
+++ MPLS_Automation.py
@@ -3,11 +3,13 @@ Router_LSR_ID=input("Enter Router LSR-ID : ")
 print("Here is your code : ")
 print("""
 mpls lsr-id {0}
-
-
-
-
-
+mpls
+mpls ldp
+q
+mpls ldp 
+q
+save 
+y
 
 """.format(Router_LSR_ID))
 input("Press enter to exit")
